ed1d6f47f9a9b93764cbcd143f869c425226489e
Vineet_Saini06 <vineetsaini.programmer@gmail.com>
2024-09-02 13:36:36 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,38 @@
+const score=400
+//console.log(score);
+
+const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // add floating value
+
+const otherNumber=138.859;
+//console.log(otherNumber.toPrecision(4)); //aprox value
+
+const hundrads=100000;
+//console.log(hundrads.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-7848)); // convert into positive value
+// console.log(Math.round(4.3)); // near the value
+// console.log(Math.ceil(7.2));  // 
+// console.log(Math.floor(8.78));
+
+// console.log(Math.max(4,3,5,4,6,4,33));
+// console.log(Math.min(4,3,5,4,6,4,33));
+
+
+console.log(Math.random()); //Random Always gives the value in b/w 0 to 1
+console.log((Math.random()*10)+1); // it will always value in between 1 to 10
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
