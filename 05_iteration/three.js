4ce3b1d517bf7491a91f5011cb47683b62cb5d40
Vineet_Saini06 <vineetsaini.programmer@gmail.com>
2024-09-16 12:36:47 +0000

    forof loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,35 @@
+ const arr=["aa","bb","c","d"]
+
+ for (const element of arr) {
+   // console.log(element);
+    
+ }
+
+ const str="hello world"
+ for (const val of str) {
+   // console.log(val);
+ }
+
+ //maps
+
+ const map=new Map()
+
+ map.set('in',"india")
+ map.set('usa',"united state of america")
+ map.set('fr',"france")
+
+ //console.log(map);
+ 
+ for (const [key,val] of map) {
+   // console.log(key,"=",val);
+    
+ }
+
+ const obj={
+    game1:"bgmi",
+    game2:"pubg"
+ }
+
+ for (const [key,valobj] of object) {
+    console.log(key,"=",val);   
+ }
